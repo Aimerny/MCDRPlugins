04a14c7e373fdd551d6f8159c2a987ff9efeeee5
Aimerny <bbbxingyu@gmail.com>
2024-09-27 20:07:39 +0800

    fix: :memo: fix path

--- view/.dumirc.ts
+++ view/.dumirc.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'MCDRPlugins'
+
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/`: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   themeConfig: {
     name: 'AMPR',
     lastUpdated: true,
